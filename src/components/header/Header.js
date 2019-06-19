b3b6cb9fd584de546ebf5126dc90361c2eba9262
Moore-ConnorS <conzo.moore1775@gmail.com>
2019-06-19 15:14:47 -0700

    tweaking animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,9 +8,9 @@ export default class Header extends React.Component {
         <div className="hero">
           <div className="image" />
           <div id="headerText">
-            <div>Hello, </div>
-            <div>I am Connor Moore</div>
-            <div>I turn ideas into reality</div>
+            <div id="hello">Hello, </div>
+            <div id="name">I am Connor Moore</div>
+            <div id="idea">I turn ideas into reality</div>
           </div>
         </div>
       </div>
